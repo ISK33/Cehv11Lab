486dedb64568d5ca1505099d084cd1ce2dea2cb1
ISK AT <48159471+ISK33@users.noreply.github.com>
2022-03-15 18:16:45 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,17 +1,18 @@
-<?php echo " <?php if (isset($_COOKIE["user"])&&isset($_COOKIE["valid"])){
-	if($_COOKIE["user"]==base64_decode("YWRtaW4=") && $_COOKIE["valid"]==base64_decode("VHJ1ZQ==")){
-	?>
-    <h1 class="my-5">Hi Admin</h1>
-	
-        <a href="logout.php" >Sign Out of Your Account</a>
+<?php
+
+ echo htmlspecialchars(" <?php if (isset("."$"."_COOKIE['user'])&&isset("."$"."_COOKIE['valid'])){\r\n");echo "<br>";;echo "<br>";
+	echo htmlspecialchars("if("."$"."_COOKIE['user']==base64_decode('YWRtaW4=') && "."$"."_COOKIE['valid']==base64_decode('VHJ1ZQ==')){");echo "<br>";
+echo "<br>";echo "<br>";
+  echo htmlspecialchars("  <h1 class='my-5'>Hi Admin</h1>");echo "<br>";echo "<br>";
+     echo htmlspecialchars("   <a href='logout.php' >Sign Out of Your Account</a>");echo "<br>";echo "<br>";
 		
-    </p>
+    
 
-
-<form action="" method="get">
-Search <input type="text" size="100" name="search"><br>
-<input type ="submit">
-</form>
-</body>
-}else echo "<br><br><br><h1>You aren't admin</h1> <a href='https://www.freeiconspng.com/img/13412' title='Image from freeiconspng.com'>
-</html>"
+echo htmlspecialchars("<form action='' method='get'>");echo "<br>";echo "<br>";
+echo htmlspecialchars("Search <input type='text' size='100' name='search'><br>");echo "<br>";echo "<br>";
+echo htmlspecialchars("<input type ='submit'>");echo "<br>";echo "<br>";
+echo htmlspecialchars("</form>");echo "<br>";echo "<br>";
+echo htmlspecialchars("</body>");echo "<br>";echo "<br>";
+echo htmlspecialchars("<?php }else echo '<br><br><br><h1>You aren't admin</h1> <a href='https://www.freeiconspng.com/img/13412' title='Image from freeiconspng.com'>
+</html>");echo "<br>";
+?>
