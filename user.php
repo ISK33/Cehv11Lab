312b33db8f11844864798df239db92751a374ca1
ISK AT <48159471+ISK33@users.noreply.github.com>
2022-03-19 23:56:18 +0200

    Update user.php

--- user.php
+++ user.php
@@ -17,11 +17,16 @@ if ($query->num_rows > 0){
 $str1= (explode("'",$_GET['id']));
 
 		if ((trim ($_SESSION['id']) == $str1[0])) {
+			echo " <video controls autoplay style='  max-width: 50%;height: 30%;margin-left: 500px;margin-right: auto;'>
+  <source src='movie.mp4' type='video/mp4'>
+  Your browser does not support the video tag.
+</video>";
               while ($row = mysqli_fetch_assoc($query)) {
-					echo " <h1>   id= ".$row["id"]."   <br> username= ".$row["username"]."<br> password= ".$row["password"]."  </h1>   <br>";
+					echo " 
+					<h1>   id= ".$row["id"]."   <br> username= ".$row["username"]."<br> password= ".$row["password"]."  </h1>   <br>";
 
 }
-}else{$row = mysqli_fetch_assoc($query); echo "<h1 >you are not ".$row["username"]."</h1>";}
+}else{$row = mysqli_fetch_assoc($query); echo "<h1 >you are not ".$row["username"]."</h1> ";}
 		}
 ?>
 </div>
